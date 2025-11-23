965eb78a7410ec0f5edafba01dd3c8e0ae4d3c0f
Ramalakshmi-AR <ramyarani.a@gmail.com>
2025-11-23 20:12:26 +0530

    Initial commit

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+]
