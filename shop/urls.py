d798e6244bc9044a6cf66ce68474d938002b7b14
Ramalakshmi-AR <ramyarani.a@gmail.com>
2025-11-24 21:39:37 +0530

    Updated project with new features / bug fixes

--- shop/urls.py
+++ shop/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path("payment-success/", views.payment_success, name="payment_success"),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('products/', views.products_page, name='products'),
+
+
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
 from django.conf import settings
 import razorpay
+from django.views.decorators.csrf import csrf_exempt
 
 # Home page and search/filter
 def home(request):
@@ -45,3 +46,47 @@ def checkout(request):
     client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
     payment = client.order.create({'amount': int(total*100), 'currency': 'INR', 'payment_capture': 1})
     return render(request, 'shop/checkout.html', {'payment': payment, 'total': total, 'razorpay_key': settings.RAZORPAY_KEY_ID})
+
+
+
+
+@csrf_exempt
+def payment_success(request):
+    if request.method == "POST":
+        razorpay_payment_id = request.POST.get('razorpay_payment_id')
+        razorpay_order_id = request.POST.get('razorpay_order_id')
+        razorpay_signature = request.POST.get('razorpay_signature')
+
+        # Store or print for debugging
+        print("Payment ID:", razorpay_payment_id)
+        print("Order ID:", razorpay_order_id)
+        print("Signature:", razorpay_signature)
+
+        # After success → clear cart
+        if 'cart' in request.session:
+            del request.session['cart']
+
+        return render(request, "shop/payment_success.html", {
+            "payment_id": razorpay_payment_id
+        })
+
+    return render(request, "shop/payment_failed.html")
+
+def products_page(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, "shop/products.html", {
+        "products": products,
+        "categories": categories
+    })
+
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def login(request):
+    return render(request, 'shop/login.html')
